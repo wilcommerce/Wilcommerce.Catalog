468f6912d8a84b4b69f73ccbadcf16f9a2f13514
albi.mori <albi.mori@gmail.com>
2017-05-13 15:18:03 +0200

    add comments, remove unnecessary using

--- src/Wilcommerce.Catalog/Models/ProductAttribute.cs
+++ src/Wilcommerce.Catalog/Models/ProductAttribute.cs
@@ -4,12 +4,24 @@ namespace Wilcommerce.Catalog.Models
 {
     public class ProductAttribute
     {
+        /// <summary>
+        /// Get or set the attribute's id
+        /// </summary>
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// Get or set the attribute's value
+        /// </summary>
         public object Value { get; set; }
 
+        /// <summary>
+        /// Get or set the attribute's product reference
+        /// </summary>
         public virtual Product Product { get; set; }
 
+        /// <summary>
+        /// Get or set the attribute's custom attribute reference
+        /// </summary>
         public virtual CustomAttribute Attribute { get; set; }
     }
 }
