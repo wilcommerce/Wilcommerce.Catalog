fc96ffe1f1bc5d451b83b2c4ae867767c7abf16c
albi.mori <albi.mori@gmail.com>
2017-10-21 16:37:30 +0200

    add comment to brand events

--- src/Wilcommerce.Catalog/Events/Brand/BrandCreatedEvent.cs
+++ src/Wilcommerce.Catalog/Events/Brand/BrandCreatedEvent.cs
@@ -3,12 +3,26 @@ using Wilcommerce.Core.Infrastructure;
 
 namespace Wilcommerce.Catalog.Events.Brand
 {
+    /// <summary>
+    /// New Brand created
+    /// </summary>
     public class BrandCreatedEvent : DomainEvent
     {
+        /// <summary>
+        /// Get the brand id
+        /// </summary>
         public Guid BrandId { get; private set; }
 
+        /// <summary>
+        /// Get the brand name
+        /// </summary>
         public string Name { get; private set; }
 
+        /// <summary>
+        /// Construct the event
+        /// </summary>
+        /// <param name="brandId">The brand id</param>
+        /// <param name="name">The brand name</param>
         public BrandCreatedEvent(Guid brandId, string name)
             : base(brandId, typeof(Models.Brand))
         {
@@ -16,6 +30,10 @@ namespace Wilcommerce.Catalog.Events.Brand
             Name = name;
         }
 
+        /// <summary>
+        /// Convert the event to string
+        /// </summary>
+        /// <returns>The converted string</returns>
         public override string ToString()
         {
             return $"[{FiredOn.ToString()}] Brand {Name} [{BrandId}] created successfully";
--- src/Wilcommerce.Catalog/Events/Brand/BrandDeletedEvent.cs
+++ src/Wilcommerce.Catalog/Events/Brand/BrandDeletedEvent.cs
@@ -3,16 +3,30 @@ using Wilcommerce.Core.Infrastructure;
 
 namespace Wilcommerce.Catalog.Events.Brand
 {
+    /// <summary>
+    /// Brand deleted
+    /// </summary>
     public class BrandDeletedEvent : DomainEvent
     {
+        /// <summary>
+        /// Get the brand id
+        /// </summary>
         public Guid BrandId { get; private set; }
 
+        /// <summary>
+        /// Construct the event
+        /// </summary>
+        /// <param name="brandId">The brand id</param>
         public BrandDeletedEvent(Guid brandId)
             : base(brandId, typeof(Models.Brand))
         {
             BrandId = brandId;
         }
 
+        /// <summary>
+        /// Convert the event to string
+        /// </summary>
+        /// <returns>The converted string</returns>
         public override string ToString()
         {
             return $"[{FiredOn.ToString()}] Brand {BrandId} deleted successfully!";
