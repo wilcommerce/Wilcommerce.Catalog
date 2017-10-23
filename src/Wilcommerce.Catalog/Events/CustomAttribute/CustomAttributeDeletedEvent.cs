39ee18ae4b3bf384ae45f5f00a2de789177f321d
albi.mori <albi.mori@gmail.com>
2017-10-23 13:01:05 +0200

    add comments to custom attribute events

--- src/Wilcommerce.Catalog/Events/CustomAttribute/CustomAttributeDeletedEvent.cs
+++ src/Wilcommerce.Catalog/Events/CustomAttribute/CustomAttributeDeletedEvent.cs
@@ -3,16 +3,30 @@ using Wilcommerce.Core.Infrastructure;
 
 namespace Wilcommerce.Catalog.Events.CustomAttribute
 {
+    /// <summary>
+    /// Attribute deleted
+    /// </summary>
     public class CustomAttributeDeletedEvent : DomainEvent
     {
+        /// <summary>
+        /// Get the attribute id
+        /// </summary>
         public Guid AttributeId { get; private set; }
 
+        /// <summary>
+        /// Construct the event
+        /// </summary>
+        /// <param name="attributeId">The attribute id</param>
         public CustomAttributeDeletedEvent(Guid attributeId)
             : base(attributeId, typeof(Models.CustomAttribute))
         {
             AttributeId = attributeId;
         }
 
+        /// <summary>
+        /// Convert the event to string
+        /// </summary>
+        /// <returns>The converted string</returns>
         public override string ToString()
         {
             return $"[{FiredOn.ToString()}] Attribute {AttributeId} deleted";
