5ab436e6e0e4722326d9a7f54a407b47d4637589
albi.mori <albi.mori@gmail.com>
2017-10-28 12:12:01 +0200

    add comments to some product events

--- src/Wilcommerce.Catalog/Events/Product/ProductDeletedEvent.cs
+++ src/Wilcommerce.Catalog/Events/Product/ProductDeletedEvent.cs
@@ -3,16 +3,30 @@ using Wilcommerce.Core.Infrastructure;
 
 namespace Wilcommerce.Catalog.Events.Product
 {
+    /// <summary>
+    /// Product deleted
+    /// </summary>
     public class ProductDeletedEvent : DomainEvent
     {
+        /// <summary>
+        /// Get the product id
+        /// </summary>
         public Guid ProductId { get; private set; }
 
+        /// <summary>
+        /// Construct the event
+        /// </summary>
+        /// <param name="productId">The product id</param>
         public ProductDeletedEvent(Guid productId)
             : base(productId, typeof(Models.Product))
         {
             ProductId = productId;
         }
 
+        /// <summary>
+        /// Convert the event to string
+        /// </summary>
+        /// <returns>The converted string</returns>
         public override string ToString()
         {
             return $"[{FiredOn.ToString()}] Product {ProductId} deleted";
--- src/Wilcommerce.Catalog/Events/Product/ProductImageAddedEvent.cs
+++ src/Wilcommerce.Catalog/Events/Product/ProductImageAddedEvent.cs
@@ -3,14 +3,32 @@ using Wilcommerce.Core.Infrastructure;
 
 namespace Wilcommerce.Catalog.Events.Product
 {
+    /// <summary>
+    /// Product image added
+    /// </summary>
     public class ProductImageAddedEvent : DomainEvent
     {
+        /// <summary>
+        /// Get the product id
+        /// </summary>
         public Guid ProductId { get; private set; }
 
+        /// <summary>
+        /// Get the image's name
+        /// </summary>
         public string Name { get; private set; }
 
+        /// <summary>
+        /// Get the image's original name
+        /// </summary>
         public string OriginalName { get; private set; }
 
+        /// <summary>
+        /// Construct the event
+        /// </summary>
+        /// <param name="productId">The product id</param>
+        /// <param name="name">The image's name</param>
+        /// <param name="originalName">The image's original name</param>
         public ProductImageAddedEvent(Guid productId, string name, string originalName)
             : base(productId, typeof(Models.Product))
         {
@@ -19,6 +37,10 @@ namespace Wilcommerce.Catalog.Events.Product
             OriginalName = originalName;
         }
 
+        /// <summary>
+        /// Convert the event to string
+        /// </summary>
+        /// <returns>The converted string</returns>
         public override string ToString()
         {
             return $"[{FiredOn.ToString()}] Image {Name} ({OriginalName}) added to product {ProductId}";
