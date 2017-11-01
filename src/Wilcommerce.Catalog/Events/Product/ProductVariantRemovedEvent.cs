1b834e33db14f8aae72794ee202f5a60d93352b5
albi.mori <albi.mori@gmail.com>
2017-11-01 10:41:44 +0100

    add product events, fix test

--- src/Wilcommerce.Catalog/Events/Product/ProductVariantRemovedEvent.cs
+++ src/Wilcommerce.Catalog/Events/Product/ProductVariantRemovedEvent.cs
@@ -3,12 +3,26 @@ using Wilcommerce.Core.Infrastructure;
 
 namespace Wilcommerce.Catalog.Events.Product
 {
+    /// <summary>
+    /// Product variant removed
+    /// </summary>
     public class ProductVariantRemovedEvent : DomainEvent
     {
+        /// <summary>
+        /// Get the product id
+        /// </summary>
         public Guid ProductId { get; private set; }
 
+        /// <summary>
+        /// Get the variant id
+        /// </summary>
         public Guid VariantId { get; private set; }
 
+        /// <summary>
+        /// Construct the event
+        /// </summary>
+        /// <param name="productId">The product id</param>
+        /// <param name="variantId">The variant id</param>
         public ProductVariantRemovedEvent(Guid productId, Guid variantId)
             : base(productId, typeof(Models.Product))
         {
@@ -16,6 +30,10 @@ namespace Wilcommerce.Catalog.Events.Product
             VariantId = variantId;
         }
 
+        /// <summary>
+        /// Convert the event to string
+        /// </summary>
+        /// <returns>The converted string</returns>
         public override string ToString()
         {
             return $"[{FiredOn.ToString()}] Variant {VariantId} removed from product {ProductId}";
--- src/Wilcommerce.Catalog/Events/Product/ProductVendorSetEvent.cs
+++ src/Wilcommerce.Catalog/Events/Product/ProductVendorSetEvent.cs
@@ -3,12 +3,26 @@ using Wilcommerce.Core.Infrastructure;
 
 namespace Wilcommerce.Catalog.Events.Product
 {
+    /// <summary>
+    /// Product vendor set
+    /// </summary>
     public class ProductVendorSetEvent : DomainEvent
     {
+        /// <summary>
+        /// Get the product id
+        /// </summary>
         public Guid ProductId { get; private set; }
 
+        /// <summary>
+        /// Get the vendor id
+        /// </summary>
         public Guid VendorId { get; private set; }
 
+        /// <summary>
+        /// Construct the event
+        /// </summary>
+        /// <param name="productId">The product id</param>
+        /// <param name="vendorId">The vendor id</param>
         public ProductVendorSetEvent(Guid productId, Guid vendorId)
             : base(productId, typeof(Models.Product))
         {
@@ -16,6 +30,10 @@ namespace Wilcommerce.Catalog.Events.Product
             VendorId = vendorId;
         }
 
+        /// <summary>
+        /// Convert the event to string
+        /// </summary>
+        /// <returns>The converted string</returns>
         public override string ToString()
         {
             return $"[{FiredOn.ToString()}] Set vendor {VendorId} to product {ProductId}";
